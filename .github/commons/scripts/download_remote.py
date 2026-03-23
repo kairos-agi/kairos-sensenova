d3535f957f2debf4421e9ee1fd9a50cf1b65e0bd
zhangyingyi <zhangyingyi@senseauto.com>
2026-03-23 16:57:39 +0800

    remove ci gt video

---
+++ .github/commons/scripts/download_remote.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import time
+import json
+import subprocess
+import signal
+def run_cmd(cmd, check=True, capture_output=True):
+    """执行命令，返回输出或抛出异常"""
+    result = subprocess.run(cmd, shell=True, capture_output=capture_output, text=True)
+    if check and result.returncode != 0:
+        print(f"Command failed: {cmd}\nSTDERR: {result.stderr}\nSTDOUT: {result.stdout}")
+        sys.exit(result.returncode)
+    return result
+
+def run_cmd_live(cmd):
+    print(f"CMD: {cmd}")
+    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, bufsize=1)
+    for line in process.stdout:
+        print(line, end='', flush=True)
+    process.wait()
+    return process.returncode
+
+def main():
+    
+    # 读取环境变量
+    remote_dir = os.environ.get('REMOTE_DIR')
+    ak = os.environ.get('AOSS_AK')
+    sk = os.environ.get('AOSS_SK')
+    job_name = os.environ.get("JOB_NAME")
+
+    remote_endpoint = "white-bucket.aoss.cn-sh-01b.sensecoreapi-oss.cn"
+    if not os.path.exists("./ads-cli"):
+        run_cmd("wget https://quark.aoss.cn-sh-01.sensecoreapi-oss.cn/ads-cli/release/v1.10.0/ads-cli")
+    run_cmd("chmod +x ads-cli")
+    remote_path = os.path.join(f"s3://{ak}:{sk}@{remote_endpoint}", remote_dir)
+    local_path = os.path.join(remote_dir)
+    local_dir_path = os.path.dirname(local_path)
+    run_cmd(f"mkdir -p {local_dir_path} || true && ./ads-cli -q cp {remote_path} {local_path}")
+    run_cmd_live(f'echo "download artifacts to {local_path}"')
+
+    # 下载gt video， 为video_checker.py 做准备
+    gt_remote_path = os.path.join(f"s3://{ak}:{sk}@{remote_endpoint}", f"kairos-ci-gt/{job_name}/output.mp4")
+    gt_local_path = os.path.join(f".github/gt_resource/{job_name}/output.mp4")
+    gt_local_dir_path = os.path.dirname(gt_local_path)
+    run_cmd(f"mkdir -p {gt_local_dir_path} || true && ./ads-cli -q cp {gt_remote_path} {gt_local_path}")
+
+if __name__ == "__main__":
+    main()
